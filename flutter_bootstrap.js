43a8f8a6c5f4a740b771b08dc1f5ec77b8febe2f
samiTechiq <agbo22455@gmail.com>
2024-09-17 23:04:04 +0100

    second commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3563905618"
+    serviceWorkerVersion: "1359913206"
   }
 });
